80a529759280b0910363b76c823775a8c7f0c310
mershark <mersharktakyi@gmail.com>
2023-12-08 08:20:54 +0000

    Create App component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,19 @@
-import "@hotwired/turbo-rails"
-import "./controllers"
+// app/javascript/application.js
+import '@hotwired/turbo-rails';
+import './controllers';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store'; 
-
-import Greeting from './components/greeting';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
+import App from './components/App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Greeting />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
